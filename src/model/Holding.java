9d32bbcddf9d52595a7e0362dc4cda3ff8771c4c
quer2 <brolyssjlegenda@gmail.com>
2019-09-12 22:48:07 -0500

    Update Holding.java

--- src/model/Holding.java
+++ src/model/Holding.java
@@ -4,6 +4,8 @@
 package model;
 
 import java.util.ArrayList;
+
+import jdk.internal.org.objectweb.asm.tree.IntInsnNode;
 import model.Clubs;
 import model.Pet;
 import model.Owner;
@@ -14,7 +16,8 @@ import model.Owner;
 public class Holding {
 
 	private String name;
-	
+	String[] arreglo = new String[9];
+	String[] arregloOwner = new String[99];
 	private ArrayList<Clubs> clubs;
 	
 	public Holding(String name) {
@@ -51,4 +54,92 @@ public class Holding {
 	public void addClubs(Clubs  clubs1){
 		clubs.add(clubs1);
 	}
+	
+	//---------------------------------------------------------
+	public void ordenarClubsporId() {
+		for(int i=0;i<recolectarToString().length;i++) {
+			for(int j=1;j<recolectarToString().length;j++) {
+				if(clubs.get(i).getId().compareToIgnoreCase(clubs.get(i).getId())<0) {
+					String aux = arreglo[i];
+					arreglo[i]=arreglo[j];
+					arreglo[j]=aux;
+					
+				}
+			}
+		}
+	}
+	
+	//------------------------------------------------------------
+	public String[] recolectarToString() {
+		for(int i=0; i<clubs.size();i++) {
+			arreglo[i]=clubs.get(i).toString();
+		}
+		return  arreglo;
+	}
+//------------------------------------------------------------------
+	
+	public String ordenarClubsporNombre() {
+		String msg="";
+		for(int i=0;i<recolectarToString().length;i++) {
+			for(int j=1;j<recolectarToString().length;j++) {
+				if(clubs.get(i).getName().compareToIgnoreCase(clubs.get(i).getName())<0) {
+					String aux = arreglo[i];
+					arreglo[i]=arreglo[j];
+					arreglo[j]=aux;
+				}
+			}
+		}
+		for(int g=0;g<arreglo[g].length();g++) {
+			msg=arreglo[g];
+		}
+		return msg;
+	}
+	//----------------------------------------------------------------
+	public String ordenarClubsporFecha() {
+		String msg="";
+		for(int i=0;i<recolectarToString().length;i++) {
+			for(int j=1;j<recolectarToString().length;j++) {
+				if(clubs.get(i).getCreationDate().compareToIgnoreCase(clubs.get(i).getCreationDate())<0) {
+					String aux = arreglo[i];
+					arreglo[i]=arreglo[j];
+					arreglo[j]=aux;
+				}
+			}
+		}
+		for(int g=0;g<arreglo[g].length();g++) {
+			msg+=arreglo[g];
+		}
+		return msg;
+	}
+	//------------------------------------------------------------------
+	
+	public String[] recolectarToStringOwner() {
+		for(int i=0; i<clubs.size();i++) {
+			arregloOwner[i]=clubs.get(i).getOwners().get(i).toString();
+		}
+		return  arregloOwner;
+	}
+	//-------------------------------------------------------------------
+	public String ordenarOwnerporId() {
+		String msg="";
+		for(int i=0;i<clubs.size();i++) {
+			for(int j=0;j<arregloOwner.length;j++) {
+				for(int f=1;f<arregloOwner.length;f++) {
+					if(clubs.get(i).getOwners().get(j).getId().compareToIgnoreCase(clubs.get(i).getOwners().get(f).getId())<0) {
+						String aux = arregloOwner[j];
+						arregloOwner[j]=arregloOwner[f];
+						arregloOwner[f]=aux;
+					}
+				}	
+			}
+		}
+		for(int g=0;g<arregloOwner[g].length();g++) {
+			msg+=arregloOwner[g];
+		}
+		return msg;
+	}
+	
+	
+	
+	
 }
